066012881fcc251cb68229e3d35c526e32d404e2
Hamsa Harcourt <hamsaharcourt@gmail.com>
2025-06-19 12:36:49 +0100

    implements extract_tx_version function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 // Implement extract_tx_version function below
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
-    todo!()
+    if raw_tx_hex.len() < 8 {
+        return Err(String::from("Transaction data too short"));
+    }
+
+    let version_bytes = &raw_tx_hex[0..8];
+    let version = u32::from_str_radix(version_bytes, 16);
+
+    match version {
+        Ok(val) => Ok(val.swap_bytes()),
+        Err(_msg) => Err(String::from("Hex decode error")),
+    }
 }
